f97a7955991302a1dd27e2b61ebeaf0924ed154d
Yumi <maecalumpiano@gmail.com>
2026-01-11 15:04:29 +0800

    added a new folder about my current notes on python crash course by eric matthes

---
+++ python crash course - eric matthes/using individual values from a list.py
@@ -0,0 +1,30 @@
+# TIY (try it yourself) 3.1 - Names:
+
+names = ["ariane", "jasmine", "jade", "erbine"]
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+
+# used title method since these are names
+
+
+# TIY 3.2 Greetings
+
+greetings = f"Hello {names[-1].title()}!"
+
+print(greetings)
+
+# to fully understand the positioning system of an item i used the -1
+
+
+# TIY 3.3: My Own Lists
+
+cravings = ["blueberry cheesecake", "leche flan", "egg pie"]
+
+statement = f"I really crave {cravings[0]}, {cravings[1]}, and {cravings[2]}, but what I really want most right now is {cravings[0]} huhuhu!"
+
+print(statement)
+
+# just some tweaks of what i have learned about lists
